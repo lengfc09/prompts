2457401b358a959c4d8d482f83129d5abb41b85b
Yangyifan.Chen <chenyyfan@gmail.com>
2025-11-30 14:00:03 +0800

    Add files via upload

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "ThePrimeagen/vim-be-good",
+    cmd = 'VimBeGood',
+  },
+  {
+  "craftzdog/solarized-osaka.nvim",
+  lazy = True,
+  priority = 1000,
+  opts = function ()
+    return {
+    transparent=True,
+    }
+  end,
+  },
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
